1f8dc914061f7ea70cac699584f4687d46cf0282
Maximus <maximus852mmm456@gmail.com>
2026-04-09 23:01:32 +0300

    dev step 2

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ class Main {
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке dev");
         int devCount = 100;
+        System.out.println("Dev version 1.0");
     }
 }
